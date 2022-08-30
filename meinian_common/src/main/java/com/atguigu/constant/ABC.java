d99ce110e88239feeff3ec85755241ad84bc3f7c
Light <2256644370@qq.com>
2022-08-30 17:22:44 +0800

    添加了ABC类，master

---
+++ meinian_common/src/main/java/com/atguigu/constant/ABC.java
@@ -0,0 +1,11 @@
+package com.atguigu.constant;
+
+/**
+ * @author light
+ * @create 2022-08-30 17:21
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("AAAAA");
+    }
+}
